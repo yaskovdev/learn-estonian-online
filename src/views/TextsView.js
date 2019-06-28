8d82a5a89e8aa2fb08d3aa231db2a0e28cfed17b
yaskovdev <yaskovdev@gmail.com>
2019-06-29 00:11:50 +0300

    Cleanup.

--- src/views/TextsView.js
+++ src/views/TextsView.js
@@ -86,33 +86,6 @@ class TextsView extends PureComponent {
                                 </div>
                             </div>
                         </div>
-
-                        <div className="col-lg-4 col-md-6 mb-4">
-                            <div className="card h-100">
-                                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                                <div className="card-body">
-                                    <h4 className="card-title">
-                                        <a href="#">{t('text', { number: 6 })}</a>
-                                    </h4>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam
-                                        aspernatur!</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-4 col-md-6 mb-4">
-                            <div className="card h-100">
-                                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                                <div className="card-body">
-                                    <h4 className="card-title">
-                                        <a href="#">{t('text', { number: 8 })}</a>
-                                    </h4>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam
-                                        aspernatur!</p>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
