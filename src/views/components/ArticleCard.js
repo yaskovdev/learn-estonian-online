75ee1ced12cf3c2a214191dd5b510a02249b23e7
yaskovdev <yaskovdev@gmail.com>
2020-03-22 19:23:57 +0200

    Fix the build.

--- src/views/components/ArticleCard.js
+++ src/views/components/ArticleCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { translate } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
 const ArticleCard = ({ number, imageUrl, description, t }) => (
     <div className="card h-100">
-        <a href="#"><img className="card-img-top" src={imageUrl} alt=""/></a>
+        <Link to={`/articles/${number}`}>
+            <img className="card-img-top" src={imageUrl} alt=""/>
+        </Link>
         <div className="card-body">
             <h4 className="card-title">
-                <a href="#">{t('article', { number })}</a>
+                <Link to={`/articles/${number}`}>{t('article', { number })}</Link>
             </h4>
             <p className="card-text">{description}</p>
         </div>
--- src/views/components/TextCard.js
+++ src/views/components/TextCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { translate } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
 const TextCard = ({ number, imageUrl, description, t }) => (
     <div className="card h-100">
-        <a href="#"><img className="card-img-top" src={imageUrl} alt=""/></a>
+        <Link to={`/texts/${number}`}>
+            <img className="card-img-top" src={imageUrl} alt=""/>
+        </Link>
         <div className="card-body">
             <h4 className="card-title">
-                <a href="#">{t('text', { number })}</a>
+                <Link to={`/texts/${number}`}>{t('text', { number })}</Link>
             </h4>
             <p className="card-text">{description}</p>
         </div>
