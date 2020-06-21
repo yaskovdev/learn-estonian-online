fb9682942d9524cdbc6c94541e48b2b4f763e0e7
yaskovdev <yaskovdev@gmail.com>
2020-06-21 21:11:34 +0300

    Add more previews.

--- src/views/LessonsView.js
+++ src/views/LessonsView.js
@@ -3,6 +3,11 @@ import { translate } from 'react-i18next'
 import Categories from './components/Categories'
 import LessonCard from './components/LessonCard'
 import lesson1 from '../lesson-1-card.jpg'
+import lesson2 from '../lesson-2-card.jpg'
+import lesson3 from '../lesson-3-card.jpg'
+import lesson4 from '../lesson-4-card.jpg'
+import lesson5 from '../lesson-5-card.jpg'
+import lesson7 from '../lesson-7-card.jpg'
 
 const LessonsView = () => (
     <div className="row">
@@ -16,7 +21,17 @@ const LessonsView = () => (
 
                 <LessonCard number="1" imageSrc={lesson1}
                     description="Возьмем оставшиеся пять падежей прямо сейчас"/>
+                <LessonCard number="2" imageSrc={lesson2}
+                    description="Возьмем оставшиеся пять падежей прямо сейчас"/>
+                <LessonCard number="3" imageSrc={lesson3}
+                    description="Возьмем оставшиеся пять падежей прямо сейчас"/>
+                <LessonCard number="4" imageSrc={lesson4}
+                    description="Возьмем оставшиеся пять падежей прямо сейчас"/>
+                <LessonCard number="5" imageSrc={lesson5}
+                    description="Возьмем оставшиеся пять падежей прямо сейчас"/>
                 <LessonCard number="6" imageSrc="https://shortpixel.com/img/slider/berries-optimized-by-shortpixel.jpg"
+                    description="Возьмем оставшиеся пять падежей прямо сейчас"/>
+                <LessonCard number="7" imageSrc={lesson7}
                     description="Возьмем оставшиеся пять падежей прямо сейчас"/>
 
             </div>
