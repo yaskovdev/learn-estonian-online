fc62d12852cf6a5521c23da26dfe78adc1c3f6b4
yaskovdev <yaskovdev@gmail.com>
2020-07-18 20:10:29 +0300

    More lessons drafts.

--- src/views/lessons/Lesson11View.js
+++ src/views/lessons/Lesson11View.js
@@ -3,7 +3,126 @@ import { translate } from 'react-i18next'
 import MarkdownPage from '../components/MarkdownPage'
 
 const source = `
+Мир не стоит на месте, и мы тоже! На предыдущем занятии мы начали с Вами рассматривать материал по предлогам и послелогам эстонского языка. Взяли подгруппу, которая касается пространственного обозначения. Сегодня же я предлагаю Вам познакомиться с оставшимися подгруппами предлогов и послелогов. Хотя уточню, слово оставшиеся здесь подразумевает не то, что я собрала все предлоги и послелоги в одном месте! Это, к сожалению, не так. Не смотря на то, что список ниже достаточно большой, он далеко не полный. Но поверьте, нам пока будет достаточно и этого. Тем более я постаралась соединить в одном материале как можно больше подтем, и тем самым охватить как можно шире все сферы нашей повседневной жизни. 
 
+Предлоги и послелоги, выражающие причину и цель действия.
+1.Послелоги, требуют падежа omastav: 
+Pärast=Tõttu – из-за, по, вследствие
+Jaoks=Tarvis - для
+Heaks – для, во благо
+Üle – за кого-то, за что-то, кому, чему
+Puhul – в честь, по случаю
+Põhjal – на основе
+Näide.
+Ta ei lähe tööle haiguse pärast. Он не идет на работу вследствие болезни.
+Selle tõttu olen ma kodus. Из-за этого я дома.
+Minu jaoks leiab ta alati aega. Для меня она всегда найдет время.
+Kasutan referaadi tarvis materjalid raamatukogust. Использую для реферата материалы из библиотеки.
+Ta teeb kõik oma pere heaks. Он делает все во благо своей семьи.
+Mina olen õnnelik sõbra üle. Я счастлива за друга.
+Teen sulle sünnipäeva puhul kingituse.Сделаю тебе подарок по случаю дня рождения.
+Mille põhjal kohtunik nii otsustab? На основе чего судья так решает?
+
+2. Предлог  tänu – благодаря, используется с падежом alaleütlev:
+Tänu töötajatele saab see maja õigeks ajaks valmis. Благодаря работникам будет этот дом вовремя готов.
+
+Предлоги и послелоги для выражения временных отношений.
+1. Послелоги, используемые с падежом omastav:
+Jooksul=kestel – в течение, во время, 
+Eest – за, «тому назад»
+Pärast - через
+Eel – перед
+Peale - после
+Näide:
+Lõpeta töö nadala jooksul! Закончи работу в течение недели! 
+Ta pole aasta kestel kordagi koolis käinud. В течение года она ни разу не была в школе.
+Mina jooksen kilomeetrit viie minuti eest. Я пробегаю километр за пять минут.
+Seminar algab kolme tunni pärast. Семинар начнется через три часа
+Päikese tõusu eel on ilm külm. Перед восходом солнца погода холодная.
+Raske töö peale tuleb puhata. После тяжелой работы следует отдыхать.
+
+2.Предлоги, требующие падежа osastav:
+Enne – перед, до
+Pärast – после 
+Enne eksamit pean veel õpiku läbi lugema. Перед (до) экзаменом должен еще учебник прочитать.
+Pärast tööd lähen ma kohvikusse. После работы пойду в кафе.
+
+3. Послелоги, используемые с падежом nimetav:
+Läbi – «весь , всё, напролет»
+Otsa – «весь, целый»
+Istume ööd läbi üleval. Сидим ночи напролет наверху.
+Restoran on päev otsa reserveeritud. Ресторан забронирован на целый день.
+
+3. Послелоги, требующие падежа seestütlev:
+Saadik – с, со
+Peale – с, со
+Alates – «начиная с». Alates может быть как послелог, так и предлог, но в любом случае используется вместе с падежом seestütlev.
+Ta on lapsepõlvest saadik linnas elanud. Он с детства живёт в городе.
+Me oleme sõbrad esimesest kohtumisest peale. Мы друзья с первой встречи.
+Ta töötab meil sügisest alates. Он работает у нас начиная с осени.
+
+Предлоги и послелоги, выражающие количество, меру, размер.
+1. Послелоги, требующие падежа omastav:
+Kaupa=viisi – по (можно перевести также творительным падежом)
+Ümber - около
+Kohta – на 
+Võrra – на 
+Näide:
+Jüri on aastate kaupa kodust eemal viibinud. Юрий не живет дома годами.
+Tüdruk võib tundude viisi raamatuid lugeda. Девочка может часами читать книги.
+Peole tuleb kahekümne inimese ümber. На праздник придет около двадцати человек.
+See on ainus korralik maja terve küla kohta. Это единственный приличный дом на всю деревню.
+Palk tõuseb viiesaja rubla võrra. Зарплата повысится на пятьсот рублей.
+
+2. Предлоги, требующие падежа omastav:
+Alla – меньше, ниже
+Üle – больше, свыше
+Ta on meil veidi alla aasta töötanud. Он у нас работает немного меньше года.
+Toas on üle viieteistkümne inimese. В комнате более пятнадцати человек.
+
+Послелоги, выражающие отношения и связь.
+1. послелоги, требующие падежа omastav:
+Kohta – о(об), про, касательно
+Poolest – в отношении, по (кому-чему)
+Suhtes - насчет, относительно, по отношению
+Meelest – по мнению (кого-чего)
+Järgi – согласно
+Näide:
+Andmed tema edasise saatuse kohta puuduvad. Сведения о его дальнейшей судьбе отсутствуют.
+Vanuse poolest on ta teistest üle.  По возрасту он опередил других.
+See kehtib ka sinu suhtes. Это действует и по отношению к тебе. (Это и тебя касается.)
+Minu meelest on Tõnis kõiges süüdi. По моему мнению, Тынис виноват во всем.
+Lisan soola suppi maitse järgi. Соль в суп добавляю по вкусу.
+
+Предлогов в русском языке существует несметное множество, точно также как и в эстонском языке. В дополнение к вышесказанным предлогам и послелогам можно выделить еще несколько небольших подгрупп.
+Дополнительные подгруппы предлогов и послелогов, выражающие следующие отношения:
+1. Орудие или средство.
+Abil – «при помощи» (послелог с падежом omastav)
+Kelle abil sa oled selle töö lõpetanud? С помощью кого (кто помог) ты закончил эту работу?
+Teel – по (послелог с падежом omastav)
+Ma helistan sulle telefoni teel. Я позвоню тебе по телефону.
+2. Замену.
+Asemel – вместо, за (послелог с падежом omastav)
+Sa pead kellegi enda asemel saatma.  Ты должен кого-то вместо себя отправить.
+Eest – вместо, за (послелог с падежом omastav)
+Ta töötab kahe eest. Он работает за двоих.
+3. Сопровождение.  
+Saatel – «в сопровождении» (послелог с падежом omastav)
+Turistid sõidavad giidi saatel hotelli.  Туристы едут в отель в сопровождении гида. 
+Koos - вместе (предлог с падежом kaasaütlev)
+Ta tuleb koos vennaga. Он приедет вместе с братом.
+4. Отсутсвие.
+Ilma – без (предлог с падежом ilmaütlev)
+Ilma tööta pole võimalik rahulikult elada. Без работы не возможно спокойно жить.
+5. Исключение.
+Peale – кроме (предлог с падежом omastav)
+Peale direktori ei tea sellest otsusest veel keegi. Кроме директора никто не знает об этом решении.
+
+Предлогов и послелогов много. Верю, что запомнить их с первого раза, мало кому удастся. Не отчаивайтесь, постепенно они все отложатся. Но, обратите обязательно внимание вот на какой факт, некоторые слова из выше перечисленных используются и как в качестве предлогов, так и послелогов, да еще и с разными падежами взаимодействуют. Будьте осторожны, значение всего сказанного может кардинально поменяться, если вы допустите в использовании этих частичек ошибку.
+Näide:
+Me kohtume aasta pärast. Мы встретимся через год. (omastav+pärast) 
+Pärast tööd läheme koju. После работы идем домой. (pärast+osastav)
+Laps puudub koolist haiguse pärast. Ребёнок отсутствует в школе из-за болезни. (omastav+pärast)
 `
 
 export default translate()(props => <MarkdownPage title={props.t('lesson', { number: 11 })} source={source}/>)
